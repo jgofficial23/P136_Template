c419ba9e6fd4683a97f2fcfabe6be57ff10a2557
jgofficial23 <73702065+jgofficial23@users.noreply.github.com>
2021-10-12 17:40:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+objects = [];
+status = "";
+
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+
+
+function start()
+{
+  
+  object_name = document.getElementById("object_name").value;
+}
+
+
+
+function draw() {
+ 
+     
+}
